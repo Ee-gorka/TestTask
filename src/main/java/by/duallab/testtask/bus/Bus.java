818fc6da3d075a5a7af72b36d06d9f76cd4bab45
Egor Martsynkovskiy <fpm.marcinko@bsu.by>
2021-03-31 22:47:13 +0300

    Cleaning data and parsing function for better readability

--- src/main/java/by/duallab/testtask/bus/Bus.java
+++ src/main/java/by/duallab/testtask/bus/Bus.java
@@ -11,19 +11,19 @@ public class Bus implements Comparable<Bus> {
     private final LocalTime departureTime;
     private final LocalTime arrivalTime;
     private final long duration;
-    private boolean arrivingAfterMidnight = false;
+    private boolean arrivingAfterMidnight;
 
     public Bus(String serviceName, LocalTime departureTime, LocalTime arrivalTime) {
         this.serviceName = serviceName;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
-        if (arrivalTime.isBefore(departureTime)) arrivingAfterMidnight = true;
+        arrivingAfterMidnight = arrivalTime.isBefore(departureTime);
         this.duration = calculateDuration(departureTime, arrivalTime);
     }
 
     private long calculateDuration(LocalTime departureTime, LocalTime arrivalTime) {
         long tmpDuration = Duration.between(departureTime, arrivalTime).toMinutes();
-        if (tmpDuration < 0) return MINUTES_IN_HOUR * HOURS_IN_DAY + tmpDuration;
+        tmpDuration = (tmpDuration < 0) ? MINUTES_IN_HOUR * HOURS_IN_DAY + tmpDuration : tmpDuration;
         return tmpDuration;
     }
 
@@ -53,23 +53,36 @@ public class Bus implements Comparable<Bus> {
             return 0;
         }
         if (this.getDepartureTime().equals(bus.getDepartureTime())) {
-            if (this.getDuration() < bus.getDuration()) {
-                return 1;
-            } else if (this.getDuration() == bus.getDuration()) {
-                if (this.getServiceName().equals(SERVICE_POSH_NAME)) return 1;
-                return -1;
-            } else {
-                return -1;
-            }
+            return compareBusesWithSameDepartureTime(bus);
         }
         if (this.getArrivalTime().equals(bus.getArrivalTime())) {
-            if (this.getDuration() < bus.getDuration()) {
-                return 1;
-            } else if (this.getDuration() > bus.getDuration()) {
-                return -1;
-            }
+            return compareBusesWithSameArrivalTime(bus);
         }
+        return compareBusesWithPossibleLaterDepartureAndEarlierArrival(bus);
+    }
+
+    private int compareBusesWithSameDepartureTime(Bus bus) {
+        if (this.getDuration() < bus.getDuration()) {
+            return 1;
+        } else if (this.getDuration() == bus.getDuration()) {
+            if (this.getServiceName().equals(SERVICE_POSH_NAME)) return 1;
+            return -1;
+        } else {
+            return -1;
+        }
+    }
+
+    private int compareBusesWithSameArrivalTime(Bus bus) {
+        if (this.getDuration() < bus.getDuration()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    private int compareBusesWithPossibleLaterDepartureAndEarlierArrival(Bus bus) {
         if (this.isArrivingAfterMidnight() != bus.isArrivingAfterMidnight()) {
+
             if (this.getDepartureTime().minusHours(1).isBefore(bus.getDepartureTime().minusHours(1)) &&
                     this.getArrivalTime().minusHours(1).isAfter(bus.getArrivalTime().minusHours(1))) {
                 return -1;
